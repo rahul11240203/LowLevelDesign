2a1ff8f8fcda65a550cff06c69639594b4294981
Rahul Sahu <rahulsahu@Rahuls-MacBook-Air.local>
2025-08-13 00:03:15 +0530

    Initial Commit

---
+++ src/basic/pack/main.java
@@ -0,0 +1,9 @@
+package basic.pack;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.print("Hello !! s dkljf");
+		System.out.println("helo");
+		System.out.println("hl");
+	}
+}
